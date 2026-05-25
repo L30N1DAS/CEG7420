29f089da152f8809c856fb0b28a6e2e5cf6226cc
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-05-25 16:13:33 -0400

    Module 10 demo

---
+++ module_10_demo/main.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void) {
+    double input = 16.0;
+    double result = sqrt(input);
+
+    printf("sqrt(%0.1f) = %0.1f\n", input, result);
+    return 0;
+}
