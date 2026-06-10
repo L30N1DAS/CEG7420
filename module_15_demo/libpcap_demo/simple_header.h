f1069cb27bf0958cb4ad4718efa25c4f481bca99
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-06-10 17:25:45 -0400

    timeval struct

--- module_15_demo/libpcap_demo/simple_header.h
+++ module_15_demo/libpcap_demo/simple_header.h
@@ -1,6 +1,11 @@
 typedef int bpf_int32;
 typedef u_int bpf_u_int32;
 
+typedef struct timeval {
+    long tv_sec;
+    long tv_usec;
+} timeval;
+
 struct pcap_pkthdr {
     struct timeval ts;      /* time stamp */
     bpf_u_int32 caplen;     /* length of portion present */
