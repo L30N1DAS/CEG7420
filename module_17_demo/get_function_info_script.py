047edcc1fe9fe2858550d6818e70bed6e8f837c1
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-06-29 21:48:53 -0400

    exit address and function size

--- module_17_demo/get_function_info_script.py
+++ module_17_demo/get_function_info_script.py
@@ -5,13 +5,16 @@
 myFuncManager = currentProgram.getFunctionManager()
 allFuncs = myFuncManager.getFunctions(True)
 for f in allFuncs:
+
+    f_body = f.getBody()
+    
     print("-"*10)
     print("name:\t\t{}".format(f.getName()))
     print("calling convention:\t\t{}".format(f.getCallingConventionName()))
     print("signature:\t\t{}".format(f.getSignature()))
     print("entry address:\t\t{}".format(f.getEntryPoint()))
-    # print("exit address:\t\t{}".format(f_body().getMaxAddress()))
-    # print("size of function body:\t\t{}".format(f_body().getNumAddresses()))
+    print("exit address:\t\t{}".format(f_body.getMaxAddress()))
+    print("size of function body:\t\t{}".format(f_body.getNumAddresses()))
     print("internal function:\t\t{}".format(not f.isExternal()))
     print("external function:\t\t{}".format(f.isExternal()))
     print("inline function:\t\t{}".format(f.isInline()))
