fe61800f966e4e319638fc9a9792fc2c455a2f84
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-06-29 22:01:40 -0400

    more function info

--- module_17_demo/get_function_info_script.py
+++ module_17_demo/get_function_info_script.py
@@ -7,7 +7,7 @@ allFuncs = myFuncManager.getFunctions(True)
 for f in allFuncs:
 
     f_body = f.getBody()
-    
+
     print("-"*10)
     print("name:\t\t{}".format(f.getName()))
     print("calling convention:\t\t{}".format(f.getCallingConventionName()))
@@ -19,3 +19,16 @@ for f in allFuncs:
     print("external function:\t\t{}".format(f.isExternal()))
     print("inline function:\t\t{}".format(f.isInline()))
     print("thunk function:\t\t{}".format(f.isThunk()))
+
+    parameter_cnt = f.getParameterCount()
+    parameter_list = f.getParameters()
+    for i in parameter_list:
+        print("parameter:\t\t{}".format(i))
+
+    local_variables = f.getLocalVariables()
+    for i in local_variables:
+        print("local variable:\t\t{}".format(i))
+
+    ret_variable = f.getReturn()
+    ret_variable_type = f.getReturnType()
+    print("return variable: {} with the type of {}".format(ret_variable, ret_variable_type))
