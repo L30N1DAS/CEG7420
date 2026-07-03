036e952a571e78589bfdb9d8cd99282882dedf50
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-03 17:55:54 -0400

    limited to 10 instructions

--- module_19_demo/get_all_instructions_script.py
+++ module_19_demo/get_all_instructions_script.py
@@ -2,7 +2,12 @@
 # @category: GhidraScripting
 # @author: Junjie Zhang
 
+cnt = 0
+
 inst = getFirstInstruction()
 while inst:
+    cnt += 1
     print("{} : {}".format(inst.getAddress(), inst))
     inst = getInstructionAfter(inst)
+    if cnt > 10:
+        break
