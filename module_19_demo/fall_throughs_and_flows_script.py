ec3dcf04e245e12cb5bf6188cc34768eff9c4a28
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-06 03:07:38 -0400

    fall-throughs and flows

---
+++ module_19_demo/fall_throughs_and_flows_script.py
@@ -0,0 +1,28 @@
+# Ghidra Scripting: Instructions
+# @category: GhidraScripting
+# @author: Junjie Zhang
+
+# get the fallthrough instructions and non-fallthrough/target instructions for J, CALL, and RET instructions.
+
+myFunc = getFunctionContaining(currentAddress)
+if myFunc:
+    print(myFunc)
+    fbody = myFunc.getBody() #fbody is an object of AddressSetView
+    myListing = currentProgram.getListing()
+    instructionIterator = myListing.getInstructions(fbody, True)
+    for inst in instructionIterator:
+
+        print("-"*10)
+
+        if inst.getMnemonicString().startswith("J") or inst.getMnemonicString().startswith("CALL") or inst.getMnemonicString().startswith("RET"):
+            print("{}\t{}".format(inst.getAddress(), inst))
+
+            addrFallThrough = inst.getFallThrough()
+            if addrFallThrough:
+                fallThroughtInst = getInstructionAt(addrFallThrough)
+                print("        fallthrough to: {}\t{}".format(addrFallThrough, fallThroughtInst))
+
+            addrOtherThanFallThrough = inst.getFlows()
+            for one in addrOtherThanFallThrough:
+                notFallThroughtInst = getInstructionAt(one)
+                print("        notfallthrough/targe/flow to: {}\t{}".format(one, notFallThroughtInst))
