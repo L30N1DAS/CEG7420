65e6d274096254f233dc64f4bd010cb531d6f7da
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-06 14:24:15 -0400

    references within function

---
+++ module_20_demo/references_within_function_script.py
@@ -0,0 +1,13 @@
+# Ghidra Scripting: References
+# @category: GhidraScripting
+# @author: Junjie Zhang
+
+myFunc = getFunctionContaining(currentAddress)
+if myFunc:
+    print(myFunc.getName())
+    fbody = myFunc.getBody()
+    for addr in fbody.getAddresses(True):
+        for i in getReferencesFrom(addr):
+            print("a ref from this address {}: {}".format(addr, i))
+        for i in getReferencesTo(addr):
+            print("a ref to this address {}: {}".format(addr, i))
