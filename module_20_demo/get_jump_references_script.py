1dc60acf569a3b99ef3fac3b84d7ea97664b8a14
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-06 15:22:32 -0400

    ref types

---
+++ module_20_demo/get_jump_references_script.py
@@ -0,0 +1,12 @@
+# Ghidra Scripting: References
+# @category: GhidraScripting
+# @author: Junjie Zhang
+
+myListing = currentProgram.getListing()
+instructionIterator = myListing.getInstructions(True)
+for inst in instructionIterator:
+    addr = inst.getAddress()
+    allRefsFromAddr = getReferencesFrom(addr)
+    for ref in allRefsFromAddr:
+        if ref.getReferenceType().isJump():
+            print("{} with the specific type of {}".format(ref, ref.getReferenceType().getName()))
