c87b9574bd53bd76efeff95093b55ba2b89f539b
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-10 18:45:06 -0400

    count: function -> entire binary

--- assignments/assignment3/scripts/basic_blocks_count_script.py
+++ assignments/assignment3/scripts/basic_blocks_count_script.py
@@ -2,18 +2,11 @@
 # @category: assignment3_scripts
 # @author: Anmol Saini
 
-# To enumerate all basic blocks in the current function.
+# To enumerate all basic blocks in a binary.
 
 from ghidra.program.model.block import BasicBlockModel
 
 myBlockModel = BasicBlockModel(currentProgram)
-currentFunc = getFunctionContaining(currentAddress)
-if currentFunc:
-    fbody = currentFunc.getBody()
-    myBasicBlocks = myBlockModel.getCodeBlocksContaining(fbody, monitor) #fbody is an instance of the AddressSetView class
-    basicBlocksCount = 0
-    for i in myBasicBlocks:
-        name = i.getName()
-        print('Name: {}, Starting Address: {}'.format(name, i.getFirstStartAddress()))
-        basicBlocksCount += 1
-    print('Total basic blocks in function {}: {}'.format(currentFunc.getName(), basicBlocksCount))
+myBasicBlocks = myBlockModel.getCodeBlocks(monitor)
+for i in myBasicBlocks:
+    print(i)
