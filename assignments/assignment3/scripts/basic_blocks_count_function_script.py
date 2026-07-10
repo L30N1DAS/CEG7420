203f48be1fa44488fdd9ede2b0ca8aff39ebd609
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-10 19:16:14 -0400

    basic blocks count in function

---
+++ assignments/assignment3/scripts/basic_blocks_count_function_script.py
@@ -0,0 +1,22 @@
+# Assignment 3: Basic Blocks in Function Count
+# @category: assignment3_scripts
+# @author: Anmol Saini
+
+# To enumerate all basic blocks in the current function.
+
+from ghidra.program.model.block import BasicBlockModel
+
+myBlockModel = BasicBlockModel(currentProgram)
+currentFunc = getFunctionContaining(currentAddress)
+
+if currentFunc:
+    fbody = currentFunc.getBody()
+    myBasicBlocks = myBlockModel.getCodeBlocksContaining(fbody, monitor) #fbody is an instance of the AddressSetView class
+    basicBlocksCount = 0
+
+    for i in myBasicBlocks:
+        name = i.getName()
+        print('Name: {}, Starting Address: {}'.format(name, i.getFirstStartAddress()))
+        basicBlocksCount += 1
+        
+    print('Total basic blocks in function {}: {}'.format(currentFunc.getName(), basicBlocksCount))
