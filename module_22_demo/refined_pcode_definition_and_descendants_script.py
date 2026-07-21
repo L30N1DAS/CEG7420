d8bddcaebb4b85bb01293e1213b819f66bf91c59
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-20 23:22:38 -0400

    refined pcode defs and descendants

---
+++ module_22_demo/refined_pcode_definition_and_descendants_script.py
@@ -0,0 +1,50 @@
+# Ghidra Scripting: P-Code
+# @category: GhidraScripting
+# @author: Junjie Zhang
+
+from ghidra.app.decompiler import *
+
+# We will get refined p-code for the current function.
+func = getFunctionContaining(currentAddress)
+myDecomp = DecompInterface()
+myDecomp.openProgram(currentProgram)
+
+if func is None:
+    print("No function contains this address.")
+    exit()
+
+decomp_results = myDecomp.decompileFunction(func, 30, monitor)
+if decomp_results is None:
+    print("Fail to decompile this function.")
+    exit()
+
+results_highFunction = decomp_results.getHighFunction()
+if results_highFunction is None:
+    print("Fail to get the high function.")
+    exit()
+
+paramSet = set()
+pcode_seq = results_highFunction.getPcodeOps() # This pcode_seq is refined pcode!
+
+for op in pcode_seq:
+    print(op)
+    output = op.getOutput()
+    if output:
+        print("\toutput varnode: {}".format(output))
+
+        inst_def = output.getDef()
+        print("\t\t It is defined by: {}".format(inst_def))
+
+        inst_descendants = output.getDescendants()
+        for i in inst_descendants:
+            print("\t\t It is used by: {}".format(i))
+            
+    inputs = op.getInputs()
+    for invar in inputs:
+        print("\tinput varnode: {}".format(invar))
+        inst_def = invar.getDef()
+        print("\t\t It is defined by: {}".format(inst_def))
+
+        inst_descendants = invar.getDescendants()
+        for i in inst_descendants:
+            print("\t\t It is used by: {}".format(i))
