47a54a02db3fe0d4462710a09904ad57871b3cde
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2026-07-24 11:24:08 -0400

    getting varnodes instead of pcodes

--- assignments/assignment4/scripts/refined_pcode_instructions_with_specific_varnode_script.py
+++ assignments/assignment4/scripts/refined_pcode_instructions_with_specific_varnode_script.py
@@ -1,4 +1,4 @@
-# Assignment 4: Refined P-code instructions with specific varnodes in main
+# Assignment 4: Refined P-code instructions with specific varnode in main
 # @category: assignment4_scripts
 # @author: Anmol Saini
 
@@ -6,6 +6,10 @@ from ghidra.app.decompiler import *
 
 # We will get refined p-code for the current function.
 func = getFunctionContaining(currentAddress)
+fbody = func.getBody() # fbody is an object of AddressSetView
+startAddress = fbody.getMinAddress()
+endAddress = fbody.getMaxAddress()
+
 myDecomp = DecompInterface()
 myDecomp.openProgram(currentProgram)
 
@@ -23,27 +27,15 @@ if results_highFunction is None:
     print("Fail to get the high function.")
     exit()
 
-pcode_seq = results_highFunction.getPcodeOps() # This pcode_seq is refined pcode!
+varnodes_highFunction = results_highFunction.getVarnodes(startAddress, endAddress) # This will populate the varnode list in the high function
 
-for op in pcode_seq:
-    print(op)
-    output = op.getOutput()
-    if output:
-        print("\toutput varnode: {}".format(output))
+for varnode in varnodes_highFunction:
+    if varnode.isAddress() and varnode.getOffset() == 0x30 and varnode.getSize() == 8:
+        print("varnode: {}".format(varnode))
 
-        inst_def = output.getDef()
-        print("\t\t It is defined by: {}".format(inst_def))
+        varnode_def = varnode.getDef()
+        print("\t It is defined by: {}".format(varnode_def))
 
-        inst_descendants = output.getDescendants()
-        for i in inst_descendants:
-            print("\t\t It is used by: {}".format(i))
-            
-    inputs = op.getInputs()
-    for invar in inputs:
-        print("\tinput varnode: {}".format(invar))
-        inst_def = invar.getDef()
-        print("\t\t It is defined by: {}".format(inst_def))
-
-        inst_descendants = invar.getDescendants()
-        for i in inst_descendants:
-            print("\t\t It is used by: {}".format(i))
+        varnode_descendants = varnode.getDescendants()
+        for i in varnode_descendants:
+            print("\t It is used by: {}".format(i))
